d3ca0ee7e0e8ea8b28cfe0dc1764f7dc249f8f31
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-05-30 16:10:51 +0200

    Updates

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
 elif os.getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
@@ -56,10 +59,13 @@ def before_request_func():
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
