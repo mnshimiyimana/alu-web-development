6b2cc71a62f879856952d3619cb62b212b78018b
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-05-30 16:04:06 +0200

    Updates

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Users views model
+""" Module of Users views
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
@@ -27,11 +27,6 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id == 'me':
-        if request.current_user:
-            return jsonify(request.current_user.to_json())
-        else:
-            abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
