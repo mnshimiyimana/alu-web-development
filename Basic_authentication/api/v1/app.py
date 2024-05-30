0785ef211ba38a42c891865cf996d122a4b37c96
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-05-30 02:10:56 +0200

    Updates

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,11 +7,20 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+if os.getenv('AUTH_TYPE') == 'auth':
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    auth = BasicAuth()
+
 
 
 @app.errorhandler(404)
